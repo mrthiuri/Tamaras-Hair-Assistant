ecf391dbeeea25788d4cfd4629b13688c517024f
Morgan Thiuri <thiurim@gmail.com>
2025-06-23 18:38:56 +0300

    changed chatbot page icon

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 main_page = st.Page(page="pages/home.py", icon="🏠", title="Home")
-chatbot_page = st.Page(page="pages/Elsy.py",title="AI Assistant",icon="🤖")
+chatbot_page = st.Page(page="pages/Elsy.py",title="AI Assistant",icon="👽")
 pg = st.navigation([main_page,chatbot_page])
 pg.run()
